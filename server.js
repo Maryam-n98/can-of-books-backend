7c5e9066d1c795e6b338d4c7ec4b797e49b13c65
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-06-01 22:08:45 +0300

    lab12

---
+++ server.js
@@ -0,0 +1,136 @@
+'use strict';
+
+//import all packages that we need
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const axios = require('axios');
+const mongoose = require('mongoose');
+
+//save all methodes inside express 
+const server = express();
+//open backend server to recieve all requests
+server.use(cors());
+
+//define the port of this backend server
+const PORT = process.env.PORT;
+
+//start listining on this port, be ready to recieve any request
+server.listen(PORT, () => {
+    console.log(`I\'m listening on PORT ${PORT}`);
+});
+
+//get this request if the route was '/'
+server.get('/', proofOfLif);
+function proofOfLif(req, res) {
+    console.log('home server route');
+    let x = 'home server route';
+    res.send(x);
+}
+
+//connect mongo with express server
+mongoose.connect('mongodb://localhost:27017/books',
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+//create collection #1 
+
+
+//create first schema for books
+const faviourateBookSchema = new mongoose.Schema({
+    bookTitle: String,
+    bookImage: String,
+    bookDescription: String,
+});
+
+//create second schema for users
+const userInfoSchema = new mongoose.Schema({
+    userName: String,
+    userEmail: String,
+    books : [faviourateBookSchema],
+
+});
+
+
+//create first model (connect the model with collection and schema)
+const userInfoModel = mongoose.model('userCollection', userInfoSchema);
+
+
+
+//create function to create the collection, model data and seading them in the database
+function seadUserCollections() {
+
+//create model data based on schema structure, 1st user
+const ahmad = new userInfoModel({
+    userName: 'Ahmad Abu Osbeh',
+    userEmail: 'ahmadabuosbeh20@gmail.com',
+    books : [
+        {
+            bookTitle: 'Mechanical Dseign',
+            bookImage: 'https://easyengineering.net/wp-content/uploads/2017/10/91nknEz3tXL.jpg',
+            bookDescription: 'Shigley\'s Mechanical Engineering Design is intended for students beginning the study of mechanical engineering design. Students will find that the text inherently directs them into familiarity with both the basics of design decisions and the standards of industrial components. It combines the straightforward focus on fundamentals that instructors have come to expect, with a modern emphasis on design and new applications. This edition maintains the well-designed approach that has made this book the standard in machine design for nearly 50 years.',
+        }
+    ,
+    {
+        bookTitle: 'Thermodynamics',
+        bookImage: 'https://qph.fs.quoracdn.net/main-qimg-d6eee7e15558e2d5b6d16197d5fd8f3c.webp',
+        bookDescription: 'This textbook is for a one semester introductory course in thermodynamics, primarily for use in a mechanical or aerospace engineering program, although it could also be used in an engineering science curriculum. The book contains a section on the geometry of curves and surfaces, in order to review those parts of calculus that are needed in thermodynamics for interpolation and in discussing thermodynamic equations of state of simple substances. It presents the First Law of Thermodynamics as an equation for the time rate of change of system energy, the same way that Newton’s Law of Motion, an equation for the time rate of change of system momentum, is presented in Dynamics. Moreover, this emphasis illustrates the importance of the equation to the study of heat transfer and fluid mechanics. New thermodynamic properties, such as internal energy and entropy, are introduced with a motivating discussion rather than by abstract postulation, and connection is made with kinetic theory.',
+    }
+
+    ]
+});
+
+//create model data based on schema structure, 2nd user
+const maryam = new userInfoModel({
+    userName: 'Mariam Alnajjar',
+    userEmail: 'mariam.mohannad98@gmail.com',
+    books : [
+        {
+            bookTitle: 'Fliud Power',
+            bookImage: 'https://i2.wp.com/mechanicalengineeringbookspdf.com/wp-content/uploads/2018/08/fluid-mechanics-by-RK-bansal.jpg?resize=238%2C300&ssl=1',
+            bookDescription: 'Design, operate, and maintain fluid and pneumatic power equipment using the expert information contained in this authoritative volume. Fluid Power Engineering presents a comprehensive approach to hydraulic systems engineering with a solid grounding in hydrodynamic theory. The book explains how to create accurate mathematical models, select and assemble components, and integrate powerful servo valves and actuators. You will also learn how to build low-loss transmission lines, analyze system performance, and optimize efficiency.',
+        },
+        {
+            bookTitle: 'Wind and Solar Power Systems',
+            bookImage: 'https://m.media-amazon.com/images/I/41+Uikg0IhL._SL500_.jpg',
+            bookDescription: 'This book provides technological and socio-economic coverage of renewable energy. It discusses wind power technologies, solar photovoltaic technologies, large-scale energy storage technologies, and ancillary power systems. In this new edition, the book addresses advancements that have been made in renewable energy: grid-connected power plants, power electronics converters, and multi-phase conversion systems. The text has been revised to include up-to-date material, statistics, and current technology trends.',
+        }
+
+    ]
+});
+
+// console.log('ahmad', ahmad);
+// console.log('maryam', maryam);
+ahmad.save();
+maryam.save();
+
+}
+
+// seadUserCollections();
+
+//recieve this request and send the response for the client
+server.get('/books', seadBooksCollections)
+
+// http://localhost:3001/books?email=ahmadabuosbeh20@gmail.com
+function seadBooksCollections(req, res) {
+    let requestedEmail = req.query.email;
+//    let responsedData =[book1,book2,book3,book4];
+    userInfoModel.find({ userEmail: requestedEmail }, function (err, userData) {
+        if (err) {
+            console.log('did not work')
+        } else {
+            
+            res.send(userData);
+
+            console.log('requestedEmail', requestedEmail);
+            console.log('userData', userData);
+            
+            
+
+            
+        }
+    });
+
+    
+
+
+}
